dbff7b747e3624aff3482b1e308e7ec3230aa6f5
kim <ksh251024@gmail.com>
2024-03-25 14:27:53 +0900

    Form 제목 변경

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 364);
+            this.ClientSize = new System.Drawing.Size(328, 364);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form1111";
             this.ResumeLayout(false);
 
         }
